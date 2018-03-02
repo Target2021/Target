d0eee67ffff08a1997a65f5911e8257ae339cb92
Flavio Parmesani <flavio76pp@hotmail.com>
2018-03-02 11:26:21 +0100

    Inserito sfondo

--- Target2021/Target2021/Form1.Designer.cs
+++ Target2021/Target2021/Form1.Designer.cs
@@ -286,6 +286,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(591, 420);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.anaMagazziniDataGridView);
