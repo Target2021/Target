ab0b6c26455437927fd3369bd4a850fd10b45c10
kristian lentino <k.lentino@hotmail.com>
2018-03-20 15:46:52 +0100

    SDSDFSA

--- Target2021/Target2021/CheckStampaggio.Designer.cs
+++ Target2021/Target2021/CheckStampaggio.Designer.cs
@@ -29,11 +29,55 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.target2021DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.target2021DataSet = new Target2021.Target2021DataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.target2021DataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.target2021DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.target2021DataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(853, 503);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // target2021DataSetBindingSource
+            // 
+            this.target2021DataSetBindingSource.DataSource = this.target2021DataSet;
+            this.target2021DataSetBindingSource.Position = 0;
+            // 
+            // target2021DataSet
+            // 
+            this.target2021DataSet.DataSetName = "Target2021DataSet";
+            this.target2021DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // CheckStampaggio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(896, 588);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "CheckStampaggio";
             this.Text = "CheckStampaggio";
+            this.Load += new System.EventHandler(this.CheckStampaggio_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.target2021DataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.target2021DataSet)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource target2021DataSetBindingSource;
+        private Target2021DataSet target2021DataSet;
     }
 }
--- Target2021/Target2021/CheckStampaggio.cs
+++ Target2021/Target2021/CheckStampaggio.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,14 @@ namespace Target2021
         public CheckStampaggio()
         {
             InitializeComponent();
+        }
+
+        private void CheckStampaggio_Load(object sender, EventArgs e)
+        {
+            string stringaconnessione = Properties.Resources.StringaConnessione;
+            string query = "SELECT * FROM Commesse WHERE=";
+            SqlConnection sql = new SqlConnection(stringaconnessione);
+            SqlCommand cmd = new SqlCommand();
         }
     }
 }
