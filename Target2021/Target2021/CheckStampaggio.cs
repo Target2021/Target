8fb2dc9a9c69a7372d6bc42c4951e01eeede7f61
kristian lentino <k.lentino@hotmail.com>
2018-03-29 14:21:01 +0200

    colorare righe al load del form

--- Target2021/Target2021/CheckStampaggio.cs
+++ Target2021/Target2021/CheckStampaggio.cs
@@ -13,7 +13,8 @@ namespace Target2021
 {
     public partial class CheckStampaggio : Form
     {
-        public int idcommessa;
+        public string idcommessa;
+        public int i = 0;
         public CheckStampaggio()
         {
             InitializeComponent();
@@ -22,7 +23,14 @@ namespace Target2021
 
         private void CheckStampaggio_Load(object sender, EventArgs e)
         {
+            int[] index=new int[10000];
             LoadStampaggio();
+            foreach(DataGridViewRow row in dataGridView1.Rows)
+            {
+                index[i] = row.Index;
+                CheckGiacenza(index[i]);
+                i++;
+            }
         }
 
         private void LoadStampaggio()
@@ -41,61 +49,93 @@ namespace Target2021
             dataGridView1.DataSource = source;
             sda.Update(dataTable);
             con.Close();
-            CheckGiacenza();
         }
 
-        private void CheckGiacenza()
+        private void CheckGiacenza(int index)
         {
-            dataGridView1.ClearSelection();
-            foreach (DataGridViewRow row in dataGridView1.Rows)
+            try
             {
-                    try
-                    {
-                        //idcommessa = Convert.ToInt32(row.Cells[0].Value);
-                        //MessageBox.Show(idcommessa.ToString());
-                        button1.Enabled = true;
-                        int quantita = Convert.ToInt32(row.Cells[7].Value);
-                        String stringa = Properties.Resources.StringaConnessione;
-                        string query = "SELECT Giacenza FROM GiacenzeMagazzini WHERE idPrime='" + Convert.ToString(row.Cells[8].Value) + "'";
-                        SqlConnection con = new SqlConnection(stringa);
-                        SqlCommand cmd = new SqlCommand(query, con);
-                        con.Open();
-                        int Giacenza = Convert.ToInt32(cmd.ExecuteScalar());
-                        con.Close();
-                        int diff = Giacenza - quantita;
-                        if (diff < 0)
-                        {
-                            //MessageBox.Show("giacenza non sufficiente, si prega di effettuare il riordino");
-                            button1.Enabled = false;
-                            button1.BackColor = Color.Red;
-                            row.DefaultCellStyle.BackColor = Color.Red;
-                        }
-                        if (Enumerable.Range(1, 10).Contains(diff))
-                        {
-                            //MessageBox.Show("materia prima in esaurimento, si prega di effettuare il riordino ", "Giacenza", MessageBoxButtons.OK);
-                            button1.Enabled = true;
-                            button1.BackColor = Color.Yellow;
-                            row.DefaultCellStyle.BackColor = Color.Yellow;
-                        }
-                        if (diff > 10)
-                        {
-                            button1.Enabled = true;
-                            button1.BackColor = Color.Green;
-                            dataGridView1.Rows[row.Index].DefaultCellStyle.BackColor = Color.Green;
+                idcommessa = Convert.ToString(dataGridView1.Rows[index].Cells[0].Value);
+                idcommessa.Replace("  ", string.Empty);
+                //MessageBox.Show(idcommessa.ToString());
+                button1.Enabled = true;
+                int quantita = Convert.ToInt32(dataGridView1.Rows[index].Cells[7].Value);
+                String stringa = Properties.Resources.StringaConnessione;
+                string query = "SELECT Giacenza FROM GiacenzeMagazzini WHERE idPrime='" + Convert.ToString(dataGridView1.Rows[index].Cells[8].Value) + "'";
+                SqlConnection con = new SqlConnection(stringa);
+                SqlCommand cmd = new SqlCommand(query, con);
+                con.Open();
+                int Giacenza = Convert.ToInt32(cmd.ExecuteScalar());
+                con.Close();
+                int diff = Giacenza - quantita;
+                if (diff < 0)
+                {                    
+                    dataGridView1.Rows[index].DefaultCellStyle.BackColor = Color.Red;
+                    button1.BackColor = Color.Red;
+                }
+                if (Enumerable.Range(1, 10).Contains(diff))
+                {
+                    MessageBox.Show("materia prima in esaurimento, si prega di effettuare il riordino ", "Giacenza", MessageBoxButtons.OK);
+                    button1.Enabled = true;
+                    button1.BackColor = Color.Yellow;
+                    dataGridView1.Rows[index].DefaultCellStyle.BackColor = Color.Yellow;
+                }
+                if (diff > 10)
+                {
+                    button1.Enabled = true;
+                    button1.BackColor = Color.Green;
+                    dataGridView1.Rows[index].DefaultCellStyle.BackColor = Color.Green;
                         
-                            //LavoraStampaggio lavoraStampaggio = new LavoraStampaggio(idcommessa);
-                            //lavoraStampaggio.Show();
+                    LavoraStampaggio lavoraStampaggio = new LavoraStampaggio(idcommessa);
+                    lavoraStampaggio.Show();
                        
-                        }
-                        dataGridView1.Refresh();
-                    }
-                    catch(Exception ex) {
-                        MessageBox.Show(ex.Message);
-                    }
-                
+                }
+            }
+            catch(Exception ex) {
+                MessageBox.Show(ex.Message);
             }
         }
+        private void CheckGiacenzaTotale()
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                try
+                {
+                    int quantita = Convert.ToInt32(row.Cells[7].Value);
+                    String stringa = Properties.Resources.StringaConnessione;
+                    string query = "SELECT Giacenza FROM GiacenzeMagazzini WHERE idPrime='" + Convert.ToString(row.Cells[8].Value) + "'";
+                    SqlConnection con = new SqlConnection(stringa);
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    con.Open();
+                    int Giacenza = Convert.ToInt32(cmd.ExecuteScalar());
+                    con.Close();
+                    int diff = Giacenza - quantita;
+                    if (diff < 0)
+                    {
+                        row.DefaultCellStyle.BackColor = Color.Red;
+                        button1.BackColor = Color.Red;
+                    }
+                    if (Enumerable.Range(1, 10).Contains(diff))
+                    {
+                        MessageBox.Show("materia prima in esaurimento, si prega di effettuare il riordino ", "Giacenza", MessageBoxButtons.OK);
+                        button1.Enabled = true;
+                        button1.BackColor = Color.Yellow;
+                        row.DefaultCellStyle.BackColor = Color.Yellow;
+                    }
+                    if (diff > 10)
+                    {
+                        button1.Enabled = true;
+                        button1.BackColor = Color.Green;
+                        row.DefaultCellStyle.BackColor = Color.Green;
+                    }
 
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
           
@@ -110,8 +150,27 @@ namespace Target2021
 
         private void button2_Click(object sender, EventArgs e)
         {
-            dataGridView1.Rows[0].DefaultCellStyle.BackColor = Color.Green;
-            dataGridView1.Rows[1].DefaultCellStyle.BackColor = Color.Yellow;
+
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            CheckGiacenzaTotale();
+        }
+
+        private void dataGridView1_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            CheckGiacenza(e.RowIndex);
+        }
+
+        private void dataGridView1_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            CheckGiacenzaTotale();
         }
     }
 }
