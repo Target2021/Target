3bc782af0a44bae55f7fd6a8866542f8da109b91
kristian lentino <k.lentino@hotmail.com>
2018-04-04 12:15:47 +0200

    modifica

--- Target2021/Target2021/CheckStampaggio.Designer.cs
+++ Target2021/Target2021/CheckStampaggio.Designer.cs
@@ -33,24 +33,19 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.target2021DataSet = new Target2021.Target2021DataSet();
             this.target2021DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.commesseBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.tableAdapterManager1 = new Target2021.Target2021DataSetTableAdapters.TableAdapterManager();
-            this.commesseBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-            this.commesseTableAdapter = new Target2021.Target2021DataSetTableAdapters.CommesseTableAdapter();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.target2021DataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.target2021DataSetBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.commesseBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.commesseBindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // button2
             // 
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button2.Location = new System.Drawing.Point(28, 19);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(37, 23);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(97, 36);
+            this.button2.Size = new System.Drawing.Size(129, 44);
             this.button2.TabIndex = 1;
             this.button2.Text = "AGGIORNA";
             this.button2.UseVisualStyleBackColor = true;
@@ -62,11 +57,11 @@
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(20, 66);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(27, 81);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(862, 439);
+            this.dataGridView1.Size = new System.Drawing.Size(1149, 540);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
@@ -82,11 +77,6 @@
             // 
             this.target2021DataSetBindingSource.DataSource = this.target2021DataSet;
             this.target2021DataSetBindingSource.Position = 0;
-            // 
-            // commesseBindingSource
-            // 
-            this.commesseBindingSource.DataMember = "Commesse";
-            this.commesseBindingSource.DataSource = this.target2021DataSet;
             // 
             // tableAdapterManager1
             // 
@@ -120,31 +110,21 @@
             this.tableAdapterManager1.UpdateOrder = Target2021.Target2021DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             this.tableAdapterManager1.UtentiTableAdapter = null;
             // 
-            // commesseBindingSource1
-            // 
-            this.commesseBindingSource1.DataMember = "Commesse";
-            this.commesseBindingSource1.DataSource = this.target2021DataSetBindingSource;
-            // 
-            // commesseTableAdapter
-            // 
-            this.commesseTableAdapter.ClearBeforeFill = true;
-            // 
             // CheckStampaggio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(891, 523);
+            this.ClientSize = new System.Drawing.Size(1188, 644);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "CheckStampaggio";
             this.Text = "CheckStampaggio";
             this.Load += new System.EventHandler(this.CheckStampaggio_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.target2021DataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.target2021DataSetBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.commesseBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.commesseBindingSource1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -153,10 +133,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.BindingSource target2021DataSetBindingSource;
         private Target2021DataSet target2021DataSet;
-        private System.Windows.Forms.BindingSource commesseBindingSource;
         private Target2021DataSetTableAdapters.TableAdapterManager tableAdapterManager1;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.BindingSource commesseBindingSource1;
-        private Target2021DataSetTableAdapters.CommesseTableAdapter commesseTableAdapter;
     }
 }
